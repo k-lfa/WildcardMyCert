5f0d94795b8a3711eec05ab17a513a9da03a79fd
k-lfa <37381068+k-lfa@users.noreply.github.com>
2024-11-09 21:12:42 +0100

    Update renew.sh

--- App/renew.sh
+++ App/renew.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-certbot certonly --dns-ovh --dns-ovh-credentials /etc/letsencrypt/ovh.ini -d example.com -d *.example.com
+certbot certonly --dns-ovh --dns-ovh-credentials /etc/letsencrypt/ovh.ini -d *.$domain
 
 # cron (tout les 2 mois)
 #0 0 2 * * /usr/local/bin/certbot renew --quiet --dns-ovh-credentials /etc/letsencrypt/ovh.ini --dns-ovh --deploy-hook "systemctl reload nginx"
